9fa2c52c42bdc2443907773bd47b455a0830fe2b
HastenX <133537562+HastenX@users.noreply.github.com>
2025-07-25 15:50:27 -0400

    Switched from Max to YAGSL
    Thank you Kevin! Switched to YAGSL after realizing that SparkMax encoders was not compatible w/ Max.
    - Imported template code
    - commented out Photon Vision references
    -  noted what buttons on driver controller does what

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,90 +4,173 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.trajectory.ExponentialProfile.Constraints;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.Constants.OIConstants;
-import frc.team1699.subsystems.DriveSubsystem;
+import frc.team1699.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import java.util.List;
+
+import java.io.File;
+
+import com.pathplanner.lib.auto.NamedCommands;
 
 public class RobotContainer {
-  private final DriveSubsystem drive = new DriveSubsystem();
 
-  CommandXboxController driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
+    private final CommandXboxController driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
 
-  public RobotContainer() {
-    configureButtonBindings();
+    private final SwerveSubsystem drivetrain = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+        "swerve/neo"));
 
-    drive.setDefaultCommand(
-        new RunCommand(
-            () -> drive.drive(
-                -MathUtil.applyDeadband(driverController.getLeftY(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(driverController.getLeftX(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(driverController.getRightX(), OIConstants.kDriveDeadband),
-                true),
-            drive));
-  }
+    SwerveInputStream driveAngularVelocity = SwerveInputStream
+        .of(drivetrain.getSwerveDrive(),
+            (() -> {
+                return driverController.getLeftY() * -1;
+            }),
+            () -> {
+                return driverController.getLeftX() * -1;
+            })
+        .withControllerRotationAxis(driverController::getRightX)
+        .deadband(OIConstants.DEADBAND)
+        .scaleTranslation(0.8)
+        .allianceRelativeControl(true);
 
+    /**
+     * Clone's the angular velocity input stream and converts it to a fieldRelative
+     * input stream.
+     */
+    SwerveInputStream driveDirectAngle = driveAngularVelocity.copy()
+        .withControllerHeadingAxis(driverController::getRightX,
+            driverController::getRightY)
+        .headingWhile(true);
 
-  private void configureButtonBindings() {
-    driverController.leftStick()
-        .whileTrue(new RunCommand(
-            () -> drive.setX(),
-            drive));
-  }
+    /**
+     * Clone's the angular velocity input stream and converts it to a robotRelative
+     * input stream.
+     */
+    SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
+        .allianceRelativeControl(false);
 
-  public Command getAutonomousCommand() {
-    // Create config for trajectory
-    TrajectoryConfig config = new TrajectoryConfig(
-        AutoConstants.kMaxSpeedMetersPerSecond,
-        AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // Add kinematics to ensure max speed is actually obeyed
-        .setKinematics(DriveConstants.kDriveKinematics);
+    SwerveInputStream driveAngularVelocityKeyboard = SwerveInputStream
+        .of(drivetrain.getSwerveDrive(),
+            () -> -driverController.getLeftY(),
+            () -> -driverController.getLeftX())
+        .withControllerRotationAxis(() -> driverController.getRawAxis(
+            2))
+        .deadband(OIConstants.DEADBAND)
+        .scaleTranslation(0.8)
+        .allianceRelativeControl(true);
+    // Derive the heading axis with math!
+    SwerveInputStream driveDirectAngleKeyboard = driveAngularVelocityKeyboard.copy()
+        .withControllerHeadingAxis(() -> Math.sin(driverController.getRawAxis(2) * Math.PI) 
+            * (Math.PI * 2),
+            () -> Math.cos(driverController.getRawAxis(2) * Math.PI)
+            * (Math.PI * 2))
+        .headingWhile(true)
+        .translationHeadingOffset(true)
+        .translationHeadingOffset(Rotation2d.fromDegrees(0));
 
-    // An example trajectory to follow. All units in meters.
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3, 0, new Rotation2d(0)),
-        config);
+    public RobotContainer() {
+        configureBindings();
+        DriverStation.silenceJoystickConnectionWarning(true);
+        NamedCommands.registerCommand("test", Commands.print("I EXIST"));
+    }
 
-    var thetaController = new ProfiledPIDController(
-        AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    private void configureBindings() {
+        // REFER TO https://gist.github.com/calebreister/8018231 FOR INDEXS-TO-BUTTONS. ALSO IGNORING TEST BUTTONS
+        // -------------------------
+        // DRIVER CONTROLLER BUTTONS
+        // -------------------------
+        // Left Joystick : Movement (AngularVelocity)
+        // A : Zero's swerve gyro towards given direction
+        // X : Fakes vision/ Limelight (eventually) reading
+        // Start && Back && right-bumper : None
+        // left bumper : Locks drivetrain
 
-    SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-        exampleTrajectory,
-        drive::getPose, // Functional interface to feed supplier
-        DriveConstants.kDriveKinematics,
+        Command driveFieldOrientedDirectAngle = drivetrain.driveFieldOriented(driveDirectAngle);
 
-        // Position controllers
-        new PIDController(AutoConstants.kPXController, 0, 0),
-        new PIDController(AutoConstants.kPYController, 0, 0),
-        thetaController,
-        drive::setModuleStates,
-        drive);
+        Command driveFieldOrientedAnglularVelocity = drivetrain.driveFieldOriented(driveAngularVelocity);
 
-    // Reset odometry to the starting pose of the trajectory.
-    drive.resetOdometry(exampleTrajectory.getInitialPose());
+        Command driveRobotOrientedAngularVelocity = drivetrain.driveFieldOriented(driveRobotOriented);
 
-    // Run path following command, then stop at the end.
-    // TODO: LINK W/ PATH PLANNER
-    return swerveControllerCommand.andThen(() -> drive.drive(0, 0, 0, false));
-  }
+        Command driveSetpointGen = drivetrain.driveWithSetpointGeneratorFieldRelative(driveDirectAngle);
+
+        Command driveFieldOrientedDirectAngleKeyboard = drivetrain.driveFieldOriented(driveDirectAngleKeyboard);
+
+        Command driveFieldOrientedAnglularVelocityKeyboard = drivetrain.driveFieldOriented(driveAngularVelocityKeyboard);
+
+        Command driveSetpointGenKeyboard = drivetrain.driveWithSetpointGeneratorFieldRelative(driveDirectAngleKeyboard);
+
+        if (RobotBase.isSimulation()) {
+            drivetrain.setDefaultCommand(driveFieldOrientedDirectAngleKeyboard);
+        } else {
+            drivetrain.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+        }
+
+        if (Robot.isSimulation()) {
+            Pose2d target = new Pose2d(new Translation2d(1, 4),
+                Rotation2d.fromDegrees(90));
+
+            drivetrain.getSwerveDrive().field.getObject("targetPose").setPose(target);
+
+            driveDirectAngleKeyboard.driveToPose(() -> target,
+                new ProfiledPIDController(5.0,
+                    0.0,
+                    0.0,
+                    new TrapezoidProfile.Constraints(5, 2)),
+                new ProfiledPIDController(5,
+                    0,
+                    0,
+                    new TrapezoidProfile.Constraints(Units.degreesToRadians(360),
+                            Units.degreesToRadians(180))));
+            driverController.start()
+                .onTrue(Commands.runOnce(() -> drivetrain.resetOdometry(new Pose2d(3, 3, new Rotation2d()))));
+            driverController.button(1).whileTrue(drivetrain.sysIdDriveMotorCommand());
+            driverController.button(2)
+                .whileTrue(Commands.runEnd(() -> driveDirectAngleKeyboard.driveToPoseEnabled(true),
+                    () -> driveDirectAngleKeyboard.driveToPoseEnabled(false)));
+
+            // driverController.b().whileTrue(
+            // drivebase.driveToPose(
+            // new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0)))
+            // );
+
+        }
+        if (DriverStation.isTest()) {
+            drivetrain.setDefaultCommand(driveFieldOrientedAnglularVelocity); // Overrides drive command above!
+
+            driverController.x().whileTrue(Commands.runOnce(drivetrain::lock, drivetrain).repeatedly());
+            driverController.y().whileTrue(drivetrain.driveToDistanceCommand(1.0, 0.2));
+            driverController.start().onTrue((Commands.runOnce(drivetrain::zeroGyro)));
+            driverController.back().whileTrue(drivetrain.centerModulesCommand());
+            driverController.leftBumper().onTrue(Commands.none());
+            driverController.rightBumper().onTrue(Commands.none());
+        } else {
+            driverController.a().onTrue((Commands.runOnce(drivetrain::zeroGyro)));
+            driverController.x().onTrue(Commands.runOnce(drivetrain::addFakeVisionReading));
+            driverController.start().whileTrue(Commands.none());
+            driverController.back().whileTrue(Commands.none());
+            driverController.leftBumper().whileTrue(Commands.runOnce(drivetrain::lock, drivetrain).repeatedly());
+            driverController.rightBumper().onTrue(Commands.none());
+        }
+    }
+
+    public Command getAutonomousCommand() {
+        // An example command will be run in autonomous
+        return drivetrain.getAutonomousCommand("New Auto");
+    }
+
+    public void setMotorBrake(boolean brake) {
+        drivetrain.setMotorBrake(brake);
+    }
 }
